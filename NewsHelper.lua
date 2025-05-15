23126b6dcfbb4dfc7fdcbecec4cd4c9c1f3dc906
Faiserx <faiser.andreich@gmail.com>
2025-05-15 09:40:34 +0300

    Add files via upload

--- NewsHelper.lua
+++ NewsHelper.lua
@@ -3474,57 +3474,54 @@ function loadVar()
 end
 
 function autoupdate(json_url, prefix, url)
-  local dlstatus = require('moonloader').download_status
-  local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
-  if doesFileExist(json) then os.remove(json) end
-  downloadUrlToFile(json_url, json,
-    function(id, status, p1, p2)
-      if status == dlstatus.STATUSEX_ENDDOWNLOAD then
-        if doesFileExist(json) then
-          local f = io.open(json, 'r')
-          if f then
-            local info = decodeJson(f:read('*a'))
-            updatelink = info.updateurl
-            updateversion = info.latest
-            f:close()
-            os.remove(json)
-            if updateversion ~= thisScript().version then
-              lua_thread.create(function(prefix)
-                local dlstatus = require('moonloader').download_status
-                local color = -1
-                sampAddChatMessage((prefix..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
-                wait(250)
-                downloadUrlToFile(updatelink, thisScript().path,
-                  function(id3, status1, p13, p23)
-                    if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
-                      print(u8:decode(string.format('Загружено %d из %d.', p13, p23)))
-                    elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
-                      print(u8:decode('Загрузка обновления завершена.'))
-                      sampAddChatMessage(u8:decode(prefix..'Обновление завершено!'), color)
-                      goupdatestatus = true
-                      lua_thread.create(function() wait(500) thisScript():reload() end)
-                    end
-                    if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
-                      if goupdatestatus == nil then
-                        sampAddChatMessage(u8:decode(prefix..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
-                        update = false
-                      end
-                    end
-                  end
-                )
-                end, prefix
-              )
-            else
-              update = false
-              print(u8:decode('v'..thisScript().version..': Обновление не требуется.'))
-            end
-          end
+  	local dlstatus = require('moonloader').download_status
+  	local json = getWorkingDirectory() .. '\\'..thisScript().name..'-version.json'
+  	if doesFileExist(json) then os.remove(json) end
+  	downloadUrlToFile(json_url, json,
+    	function(id, status, p1, p2)
+      	if status == dlstatus.STATUSEX_ENDDOWNLOAD then
+        	if doesFileExist(json) then
+				local f = io.open(json, 'r')
+				if f then
+					local info = decodeJson(f:read('*a'))
+					updatelink = info.updateurl
+					updateversion = info.latest
+					f:close()
+					os.remove(json)
+						if updateversion ~= thisScript().version then
+						lua_thread.create(function(prefix)
+							local dlstatus = require('moonloader').download_status
+							local color = -1
+							sampAddChatMessage((tag..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion), color)
+							wait(250)
+							downloadUrlToFile(updatelink, thisScript().path,
+							function(id3, status1, p13, p23)
+								if status1 == dlstatus.STATUS_DOWNLOADINGDATA then
+									print(u8:decode(string.format('Загружено %d из %d.', p13, p23)))
+								elseif status1 == dlstatus.STATUS_ENDDOWNLOADDATA then
+									sampAddChatMessage(u8:decode(tag..'Обновление завершено!'), color)
+									goupdatestatus = true
+									lua_thread.create(function() wait(500) thisScript():reload() end)
+								end
+								if status1 == dlstatus.STATUSEX_ENDDOWNLOAD then
+									if goupdatestatus == nil then
+										sampAddChatMessage(u8:decode(tag..'Обновление прошло неудачно. Запускаю устаревшую версию..'), color)
+										update = false
+									end
+								end
+							end
+							)
+							end, prefix
+							)
+						else
+							update = false
+						end
+				end
         else
-          print(u8:decode('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..url))
-          update = false
-        end
-      end
+          	update = false
+        	end
+      	end
     end
-  )
-  while update ~= false do wait(100) end
+  	)
+  	while update ~= false do wait(100) end
 end
